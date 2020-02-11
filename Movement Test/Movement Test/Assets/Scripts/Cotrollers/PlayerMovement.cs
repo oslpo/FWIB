8cddc43686ae17c33e75d47e163cc0d892e39385
SDeWever <shelby.dewever@gmail.com>
2020-02-10 23:19:47 -0600

    Visual Updates
    Added Moving Sun, character models, and minor enemy AI

---
+++ Movement Test/Movement Test/Assets/Scripts/Cotrollers/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private CharacterController characterController;
+    private Animator animator;
+
+    public float moveSpeed = 7;
+    public float turnSpeed = 150;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        animator.SetFloat("Speed", vertical);
+
+        transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
+
+        
+            characterController.SimpleMove(transform.forward * moveSpeed * vertical);
+        
+    }
+}
