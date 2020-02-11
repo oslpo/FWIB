f26f33642813ec90338b16f3eeba3bb9e44a2a7b
Antonio Herrera <37684203+Azureknight117@users.noreply.github.com>
2020-02-11 03:14:45 -0600

    minor stuff

---
+++ For When I Breath/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public List<Item> listOfItems;
+    public int maxListSize;
+
+
+    public void AddToList(Item item)
+    {
+        if (listOfItems.Count < maxListSize)
+        {
+            listOfItems.Add(item);
+        }
+        else
+        {
+            Debug.Log("INVENTORY FULL");
+        }
+    }
+    
+}
---
+++ For When I Breath/Assets/Scripts/Items/HealthKit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthKit : Item
+{
+    public float restoreAmount;
+
+    GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+    public override void Use()
+    {
+        player.GetComponent<PlayerManager>().HealDamage(restoreAmount);
+    }
+}
