328420f9abce6c2fd294bd1fff69ab7d9b5ff157
SDeWever <shelby.dewever@gmail.com>
2020-03-12 09:22:23 -0500

    Added Enemy AI / some script organization

---
+++ For When I Breath/Assets/Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+public class Enemy : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+    }
+
+}
+*/
---
+++ For When I Breath/Assets/Scripts/Cotrollers/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float sensitivity = 1f;
+    public float maxLook = 10;
+    public float minLook = -10;
+
+
+    private CinemachineComposer composer;
+    
+
+    void Start()
+    {
+        composer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineComposer>();
+    }
+    
+    void Update()
+    {
+        float vertical = Input.GetAxis("Mouse Y") * sensitivity;
+        composer.m_TrackedObjectOffset.y += vertical;
+        composer.m_TrackedObjectOffset.y = Mathf.Clamp(composer.m_TrackedObjectOffset.y, minLook, maxLook);
+    }
+}
