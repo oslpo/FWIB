5cc7f79e7542de89f9b2781bf644ca029d8b11a8
Antonio Herrera <37684203+Azureknight117@users.noreply.github.com>
2020-04-29 11:39:52 -0500

    Finished the item spawn system: There is a main item spawner object, and a item spawn point object. Place the spawn points around wherever you want an item and put the items you want to spawn there in the item object. Then put the spawnpoints on the main item spawner object and set the spawn rate for the items.

---
+++ For When I Breath/Assets/Scripts/Items/ItemSpawnPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnPoint : MonoBehaviour
+{
+    public List<GameObject> spawnableItems;
+    public GameObject item;
+    //[HideInInspector]
+    bool hasSpawned = false;
+
+    private void FixedUpdate()
+    {
+        if(item == null)
+        {
+            hasSpawned = false;
+        }
+    }
+
+    public void spawnItem ()
+    {
+        if(!hasSpawned)
+        {
+            System.Random rand = new System.Random();
+            int n = rand.Next(spawnableItems.Count);
+            item = Instantiate(spawnableItems[n], gameObject.transform);
+            hasSpawned = true;
+        }
+    }
+}
